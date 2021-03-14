bcba4a04f02391aa2ffc3930a12c02a1fc8abe3a
Shivashankar-741 <shiva1894300@gmail.com>
2021-03-14 13:35:12 +0530

    changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 
-// const url = process.env.EMPLOYEE_MANAGER;
-const url = "http://localhost:4000/posts";
+const url = "https://employeemanager741.herokuapp.com/posts";
+// const url = process.env.EMPLOYEE_MANAGER_URL;
+// const url = "http://localhost:4000/posts";
 
 export const createPost = (newPost) => axios.post(url, newPost);
 export const fetchPosts = () => axios.get(url);
